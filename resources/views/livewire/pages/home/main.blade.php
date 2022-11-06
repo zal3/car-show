db2aaf05bbec4acf38c394f8bfc79eac19b48eea
zal3 <zainabamjedlatef31@gmail.com>
2022-11-06 10:38:46 +0300

    home

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -4,11 +4,32 @@
         <section class="home" id=home>
             <div class="text">
                 <h1><span>المعرض</span> الدولي<br> للسيارات</h1>
-                <h5>للحصول على أفضل سيارة في المدينة</h5>
+                <h5>للحصول على أفضل سيارة في المدينة
+                    <br> بأفضل سعر وجودة
+
+                </h5>
                 <div class="app-stores">
-                    <img src="{{ asset('img/ios.png') }}" alt="">
-                    <img src="{{ asset('img/play.png') }}" alt="">
+                    <button class="btn"> ابدا رحلتك </button>
+                    <button class="btn"> السيارات </button>
                 </div>
+            </div>
+            {{-- form-container --}}
+            <div class="form-container">
+                <form action="">
+                    <div class="input-box">
+                        <span>الموقع</span>
+                        <input type="search" name="" id="" placeholder="ابحث عن مدينة">
+                    </div>
+                    <div class="input-box">
+                        <span>اختر الوقت</span>
+                        <input type="date" name="" id="" >
+                    </div>
+                    <div class="input-box">
+                        <span> وقت الاعادة</span>
+                        <input type="date" name="" id="" >
+                    </div>
+                    <input type="submit" name="" id="" class="btn">
+                </form>
             </div>
         </section>
     </div>
@@ -16,7 +37,8 @@
 <style scoped>
     .home {
         width: 100%;
-        min-height: 100vh;
+        top: 80px;
+        min-height: 115vh;
         position: relative;
         display: grid;
         align-items: center;
@@ -29,6 +51,7 @@
         background-inverse: right;
         position: absolute;
         -webkit-transform:scaleX(-1);
+        display: grid;
     }
 
 
@@ -49,10 +72,67 @@
 
     .app-stores {
         display: flex;
+        gap: 1rem;
+        margin-top: 30px;
+        margin-right: 30px;
     }
 
-    .app-stores img {
-        width: 100px;
-        margin-right: 1rem corsor: pointer;
+    .app-stores .btn {
+        width: 10%;
+        margin-right: 1rem;
+        corsor: pointer;
+        background: #ED563B;
+        padding: 15px 20px 15px 20px  ;
+        color: rgb(255, 255, 255);
+    }
+    .app-stores .btn:hover {
+        border-color: #eb3616;
+    background-color: #df3a1d;
+    box-shadow: 0 4px 50px #d3543e;
+    }
+
+    /* form-container */
+    .form-container form {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        gap: 1rem;
+        position: absolute;
+        bottom: 8rem;
+        left: 400px;
+        background: #fff;
+        padding: 20px;
+        border-radius: 0.5rem;
+    }
+    .input-box {
+        flex: 1 1 7rem;
+        flex-direction: column;
+        display: flex;
+    }
+    .input-box span {
+        font-weight: 500;
+    }
+    .input-box input {
+        padding: 7px;
+        outline: none;
+        background: #eeeff1;
+        border-radius: 0.5rem;
+        outline: none;
+        font-size: 1rem;
+    }
+    .form-container form .btn {
+        flex : 1 1 7rem;
+        padding: 10px 36px;
+        margin-top: 1rem;
+        border: none;
+        border-radius: 0.5rem;
+        background: #474fa0;
+        color: #fff;
+        font-size: 1rem;
+        font-weight: 500;
+
+    }
+    .form-container form .btn:hover{
+        background: #ED563B;
     }
 </style>
