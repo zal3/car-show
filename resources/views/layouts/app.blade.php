98ed44011990e0f82de7ec6b8ca84e7a8bc9fcc9
zal3 <zainabamjedlatef31@gmail.com>
2022-11-09 17:50:55 +0300

    s

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,13 +60,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    {{-- <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}">
 
 
     @livewireStyles
 
     <!-- Scripts -->
-    {{-- <script src="{{ mix('resources/js/app.js') }}" defer></script> --}}
+    <script src="{{ mix('resources/js/app.js') }}" defer></script>
 </head>
 
 <body id="home" class="bg-white h-screen home " dir="rtl">
