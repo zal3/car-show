27cae8f52b2cb9ab7285df29796d72750198270c
anwar226 <117461869+anwar226@users.noreply.github.com>
2022-11-10 19:33:28 +0300

    an

--- resources/views/livewire/pages/car/main.blade.php
+++ resources/views/livewire/pages/car/main.blade.php
@@ -2,13 +2,13 @@
         <div class="flex justify-between items-center ">
             <h2 class=" text-3xl text-black font-bold pt-20 mr-10 pb-20">انواع السيارات</h2>
         </div>
-        <div class="popular__container container swiper pb-5 "data-aos="fade-left ">
+        <div class="popular__container container swiper pb-5  "data-aos="fade-left ">
             <div class="swiper-wrapper">
               
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray  ">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car1.jpg') }}" alt="" class="popular__img ">
+                            <img src="{{ asset('img/cars/car1.jpg') }}" alt="" class="popular__img rounded-3xl  ">
                             <div class="content">
                     <h3 class="new flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange pt-4">
@@ -19,15 +19,15 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold ">check out</a>
+                    <a href="#" class="btn flex justify-center font-bold bg-orange hover:bg-orange-700   rounded-full ">check out</a>
                 </div>
                         </article>
                     </div>
                     
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car4.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car4.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -38,14 +38,14 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div>
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car2.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car2.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="new flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -56,14 +56,14 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div> 
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car3.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car3.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="nwe flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -74,7 +74,7 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div>
@@ -84,9 +84,9 @@
             <div class="swiper-wrapper">
               
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray  ">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car1.jpg') }}" alt="" class="popular__img ">
+                            <img src="{{ asset('img/cars/car1.jpg') }}" alt="" class="popular__img rounded-3xl ">
                             <div class="content">
                     <h3 class="new flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange pt-4">
@@ -97,15 +97,15 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold ">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full ">check out</a>
                 </div>
                         </article>
                     </div>
                     
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car4.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car4.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -116,14 +116,14 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div>
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car2.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car2.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="new flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -134,14 +134,14 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div> 
                     <div class="swiper-slide  ss">
-                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray">
+                        <article class=" popular__card bg-gray px-6 py-6  overflow-hidden relative w-65 bg-gray rounded-3xl hover:bg-yellow-300 ">
                             <div class="shape shape__smaller"></div>
-                            <img src="{{ asset('img/cars/car3.jpg') }}" alt="" class="popular__img">
+                            <img src="{{ asset('img/cars/car3.jpg') }}" alt="" class="popular__img rounded-3xl">
                             <div class="content">
                     <h3 class="nwe flex justify-center">new model</h3>
                     <div class="stars flex justify-center  text-orange  pt-4">
@@ -152,7 +152,7 @@
                         <i class="fas fa-star-half-alt"></i>
                     </div>
                     <div class="price flex justify-center pt-4 pb-4"> /-$55,000</div>
-                    <a href="#" class="btn flex justify-center bg-orange font-bold">check out</a>
+                    <a href="#" class="btn flex justify-center bg-orange font-bold hover:bg-orange-700   rounded-full">check out</a>
                 </div>
                         </article>
                     </div>
