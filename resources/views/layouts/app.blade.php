4e3413b8e93a0dea4a68466ed5be07aa2a8fa5c2
zal3 <zainabamjedlatef31@gmail.com>
2022-11-11 18:06:28 +0300

    ش

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>  المعرض الدولي للسيارات</title>
     <style>
         .home {
             font-family: 'Changa', sans-serif;
