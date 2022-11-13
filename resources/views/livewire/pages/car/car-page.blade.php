abcc30376b752d3464e7312dd62f2f115c6d4a24
anwar226 <117461869+anwar226@users.noreply.github.com>
2022-11-14 01:25:28 +0300

    an

--- resources/views/livewire/pages/car/car-page.blade.php
+++ resources/views/livewire/pages/car/car-page.blade.php
@@ -1,23 +1,21 @@
 <div>
-    <div class="sm:mb-10 lg:grid lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full">
+    <div class="sm:mb-10 lg:grid lg:grid-cols-5 md:grid-cols-none md:bg-gray-300 bg-gray-300 lg:bg-white lg:h-full ">
         <div
-            class=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
-          
-            <h1 class="mt-5 font-bold text-lg lg:mt-7"> </h1>
-            <h1 class="font-bold text-lg text-gray-600">Get started today!</h1>
-            <h1 class="text-lg text-gray-600 text-justify pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo  .</h1>
-                
-             <a href="{{ route('car') }}" class="hover:shadow-xl hover:scale-105  mt-5   bg-gray-600 p-3 shadow-2xl rounded-xl text-white font-bold hover:bg-gray-800">الرجوع 
+            class=" flex flex-col  justify-between items-center px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white" data-aos="fade-up" data-aos-delay="200">
+            <h1 class="font-bold text-lg text-gray-600 pb-5">اسم السيارة: جكساره</h1>
+            <h1 class="font-bold text-lg text-gray-600  pb-5">لون السيارة: اسود</h1>
+            <h1 class="font-bold text-lg text-gray-600  pb-5"> دولة الاستيراد: امريكا</h1>
+            <h1 class="font-bold text-lg text-gray-600  pb-5">سعر السيارة: 100000000</h1>
+             
+             <a href="{{ route('car') }}" class=" hover:shadow-xl hover:scale-105  mt-40 p-5  bg-gray-600   shadow-4xl rounded-xl text-white font-bold hover:bg-gray-800">الرجوع 
              </a>
         </div>
 
         <div class="hidden relative lg:block  lg:col-span-3">
-            <img class="absolute inset-0 w-full h-full pl-5 object-cover object-center rounded-3xl  hover:translate-y-1 "
+            <img class="absolute inset-0 w-full h-full pl-2  object-cover object-center rounded-3xl  hover:translate-y-1 "
                 src=" {{ asset('img/cars/car6.jpg') }}" alt="Ad- woman on a beach ">
 
-                
+
         </div>
     </div>
 </div>
