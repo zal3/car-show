d3fa193645c06d90ef61e5faa2523c139c4c63aa
anwar226 <117461869+anwar226@users.noreply.github.com>
2022-11-14 00:02:59 +0300

    an

--- resources/views/livewire/pages/car/car-page.blade.php
+++ resources/views/livewire/pages/car/car-page.blade.php
@@ -3,7 +3,7 @@
         <div
             class=" px-10 py-10 max-w-md m-auto lg:col-span-2 mt-20 mb-20 shadow-xl rounded-xl lg:mt-10 md:shadow-xl md:rounded-xl lg:shadow-none lg:rounded-none lg:w-full lg:mb-10 lg:px-5 lg:pt-5 lg:pb-5 lg:max-w-lg bg-white">
           
-            <h1 class="mt-5 font-bold text-lg lg:mt-7"></h1>
+            <h1 class="mt-5 font-bold text-lg lg:mt-7"> </h1>
             <h1 class="font-bold text-lg text-gray-600">Get started today!</h1>
             <h1 class="text-lg text-gray-600 text-justify pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -16,6 +16,8 @@
         <div class="hidden relative lg:block  lg:col-span-3">
             <img class="absolute inset-0 w-full h-full pl-5 object-cover object-center rounded-3xl  hover:translate-y-1 "
                 src=" {{ asset('img/cars/car6.jpg') }}" alt="Ad- woman on a beach ">
+
+                
         </div>
     </div>
 </div>
