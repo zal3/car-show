800f025da0a7638dbac45bea0c15b49c31fcfbe6
zal3 <102377778+zal3@users.noreply.github.com>
2022-12-10 20:44:25 +0300

    a

--- app/Http/Livewire/Pages/Car/Admin/Add.php
+++ app/Http/Livewire/Pages/Car/Admin/Add.php
@@ -56,6 +56,7 @@ class Add extends Component
                 ]);
                 redirect()->route('car');
         }
+
     public function render()
     {
         return view('livewire.pages.car.admin.add');
