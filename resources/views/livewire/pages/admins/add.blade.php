0f3fa1794d6b2b2add4bafccc55b5a81d0ea8196
zal3 <102377778+zal3@users.noreply.github.com>
2022-12-15 02:17:16 +0300

    a

--- resources/views/livewire/pages/admins/add.blade.php
+++ resources/views/livewire/pages/admins/add.blade.php
@@ -31,7 +31,7 @@
                         <label class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             الايميل
                         </label>
-                        <input wire:model.defer="user.email" type="text"
+                        <input wire:model.defer="user.email" type="email"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  "
                             placeholder="الايميل" required>
                         @error('user.email')<span class="text-red-500">{{ $message }}</span> @enderror
