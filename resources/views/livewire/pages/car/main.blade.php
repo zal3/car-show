e7881b185da5e3ec76fa56bc435defba6865b9e0
zal3 <102377778+zal3@users.noreply.github.com>
2022-12-18 15:30:36 +0300

    car

--- resources/views/livewire/pages/car/main.blade.php
+++ resources/views/livewire/pages/car/main.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-4 ">
-    <div class="flex  justify-between  items-center   m-8 mb-0 p-6 pb-0  ">
-        <div class=" text-3xl  text-black font-bold  mr-12 ">انواع السيارات</div>
+    <div class="flex  justify-between  items-center   md:m-8 m-3 p-3 mb-0 md:p-6 pb-0  ">
+        <div class="text-md md:text-3xl  text-black font-bold  mr-12 ">انواع السيارات</div>
         <div class="flex  justify-between items-center gap-3">
             <!-- filter -->
             
@@ -24,15 +24,19 @@
             @endadmin
         </div>
     </div>
-    <div class="grid grid-cols-4 gap-6 items-center justify-center px-24 py-12">
-        @forelse ($cars as $car)
+    <div  class=" flex items-center justify-center mx-auto">
+    <div class="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-6 items-center justify-center mx-auto px-7 sm:px-15 md:px-15 py-12">
+       
+    
+    @forelse ($cars as $car)
         <livewire:pages.car.card :car="$car" key="{{ now() }}" />
 
         @empty
-        <div class="px-2 py-4  mb-4 text-2xl text-gray-700 bg-primary-100 rounded-lg" role="alert">
+        <div class="px-2 py-4  mb-4 text-lg md:text-2xl text-gray-700 bg-primary-100 rounded-lg" role="alert">
             لم يتم اضافة سيارات ليتم عرضها
         </div>
         @endforelse
+    </div>
     </div>
 
 </div>
--- resources/views/livewire/ui/filter-projects.blade.php
+++ resources/views/livewire/ui/filter-projects.blade.php
@@ -1,10 +1,10 @@
 <div>
-                <button id="filterHome" data-dropdown-toggle="dropdownFilterHome" data-dropdown-placement="right"
+                <button id="filterHome" data-dropdown-toggle="dropdownFilterHome"  data-dropdown-placement="bottom"
                     class="p-2 py-1 duration-150 ease-in delay-75 rounded-lg  text-gray-600 hover:text-gray-900">
                     <i class="text-xl fa-solid fa-filter"></i>
                 </button>
 
-                <div id="dropdownFilterHome" class="hidden   rounded-lg     ">
+                <div id="dropdownFilterHome" class="hidden rounded-lg ">
                     <div>
                         <div class="grid grid-cols-2   gap-2 ">
                             <select wire:model="model"
